8f0f2ddf769aada31fe0a72efa843c7abec734aa
Rupesh Gupta <rupeshgupta369@gmail.com>
2021-09-02 02:12:47 +0530

    style : style upated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import Notelist from "./components/NotesList"
+import { useState } from "react"
+import { nanoid } from "nanoid"
+
 const App = () => {
+
+  const [notes, setNotes] = useState([{
+    id: nanoid(),
+    text: "This is my first note",
+    date: 1 / 9 / 2021
+  },
+  {
+    id: nanoid(),
+    text: "This is my second note",
+    date: 2 / 9 / 2021
+  }, {
+    id: nanoid(),
+    text: "This is my third note",
+    date: 3 / 9 / 2021
+  },])
   return <div className="container">
-    <Notelist />
+    <Notelist notes={notes} />
   </div>
 }
 export default App
